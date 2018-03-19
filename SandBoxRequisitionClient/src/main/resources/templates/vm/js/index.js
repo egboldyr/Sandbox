0b4de6d0fad4f617275ad8fdb5ddacf2d1b0f7e7
egboldyr <egboldyr>
2018-03-19 13:30:52 +0200

    SandBoxRequisitionClient (Module) - Frontend Update Added: VM Template, CSS, JS

--- SandBoxRequisitionClient/src/main/resources/templates/vm/js/index.js
+++ SandBoxRequisitionClient/src/main/resources/templates/vm/js/index.js
@@ -1,3 +1,22 @@
 /**
  * Created by egboldyr on 19.03.18.
  */
+
+function createRequisition(event) {
+    jQuery.ajax({
+        type: 'POST',
+        url: '/new_requisition',
+        data: {
+            name: jQuery('#name').val(),
+            phone: jQuery('#phone').val(),
+            email: jQuery('#email').val(),
+            comment: jQuery('#comment').val()
+        },
+        success: function () {
+            event.preventDefault();
+        },
+        error: function (errorThrown) {
+            console.log(errorThrown);
+        }
+    })
+}
