a2e45f652750b9e08ba305d720de535df9173c94
egboldyr <egboldyr>
2018-03-19 18:00:08 +0200

    SandBoxClient - Spring Settings Update

--- SandBoxClient/src/main/java/web/SandboxClientBoot.java
+++ SandBoxClient/src/main/java/web/SandboxClientBoot.java
@@ -1,15 +1,18 @@
 package web;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by EGBoldyr on 14.03.18.
  */
 
 @SpringBootApplication
+@ComponentScan("web")
 public class SandboxClientBoot {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(SandboxClientBoot.class, args);
     }
 }
