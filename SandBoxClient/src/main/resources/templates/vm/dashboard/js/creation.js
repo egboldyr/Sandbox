ec2aa67ff70de684c1d5b019237e3accb7ea21c0
egboldyr <egboldyr>
2018-03-21 11:22:43 +0200

    SandBoxClient (Module) - (FrontEnd) dashboard.html - Update Added: - View requisitions functional - New Client creation functional

--- SandBoxClient/src/main/resources/templates/vm/dashboard/js/creation.js
+++ SandBoxClient/src/main/resources/templates/vm/dashboard/js/creation.js
@@ -1,9 +1,12 @@
 function createRequisitionTable() {
-    jQuery('#workspace').empty();
+    jQuery('#workspace')
+        .empty()
+        .append('<table id="requisitions">')
+        .append('<div id="forms"></div>');
 
-    jQuery('#workspace').append('<table id="requisitions">');
-    jQuery('#requisitions').append('<caption>Requisitions</caption>')
-    jQuery('#requisitions').append('<thead><tr id="header"></tr></thead>');
+    jQuery('#requisitions')
+        .append('<caption>Requisitions</caption>')
+        .append('<thead><tr id="header"></tr></thead>');
 
     jQuery('#header')
         .append('<th>ID</th>')
@@ -16,30 +19,75 @@ function createRequisitionTable() {
         .append('<th>ACTIONS</th>');
 }
 
-function createRequisitionProcessForm() {
-    jQuery('#workspace').append('<div id="requisition_form"></div>');
+function createRequisitionProcessForm(name, phone, email, comment) {
+    jQuery('#forms')
+        .empty()
+        .append('<form id="requisition_form" class="form_view"></form>');
 
+    jQuery('#requisition_form')
+        .append('<p>Name </p>')
+        .append('<label>' + name + '</label><br/>')
+        .append('<p>Phone</p>')
+        .append('<label>' + phone + '</label><br/>')
+        .append('<p>Email</p>')
+        .append('<label>' + email + '</label><br/>')
+        .append('<p>Comment</p>')
+        .append('<label>' + comment + '</label><br/>');
 }
 
-function processRequisitions(jsonResult) {
-    console.log(jsonResult);
+function createClientCreationForm() {
+    jQuery('#forms')
+        .append('<form id="client_form" class="form_client"></form>');
 
+    jQuery('#client_form')
+        .append('<div id="ns"></div>')
+        .append('<br/><br/><br/><br/><br/><br/>') /*Костыли :(*/
+        .append('<p>Phone</p>')
+        .append('<input id="phone" type="text">')
+        .append('<p>Email</p>')
+        .append('<input id="email" type="text">')
+        .append('<input type="button" value="Create Client" onclick="return createClient()">');
+
+    jQuery('#ns')
+        .append('<div class="ns_left"><p>Name:</p><input id="name" type="text"></div>')
+        .append('<div class="ns_right"><p>Surname:</p><input id="surname" type="text"></div>');
+}
+/*********************************************************************************************************************/
+function processRequisition(btn) {
+    var requisition = btn.parentNode.parentNode;
+    createRequisitionProcessForm(
+        jQuery(requisition).find("#req_name").html(),
+        jQuery(requisition).find("#req_phone").html(),
+        jQuery(requisition).find("#req_email").html(),
+        jQuery(requisition).find("#req_comment").html()
+    );
+    /*Меняем статус заявки в случае с откытием к обработке*/
+    requisitionStatusUpdate(jQuery(requisition).find("#req_id").html(), "PROCESS");
+    createClientCreationForm();
+}
+/*********************************************************************************************************************/
+function processRequisitions(jsonResult) {
     var result = jQuery.parseJSON(jsonResult);
     jQuery('#requisitions').append('<tbody id="tblBody"></tbody>');
     for (var i = 0; i < result.length; i++) {
-        jQuery('#tblBody')
-            .append('<tr>')
-            .append('<td>' + result[i].id + '</td>')
-            .append('<td>' + result[i].status + '</td>')
-            .append('<td>' + result[i].name + '</td>')
-            .append('<td>' + result[i].phone + '</td>')
-            .append('<td>' + result[i].email + '</td>')
-            .append('<td>' + result[i].comment + '</td>')
-            .append('<td>' + result[i].date + '</td>');
+        jQuery('#tblBody').append('<tr id="cur_row'+ i +'"></tr>')
+        jQuery('#cur_row' + i)
+            .append('<td id="req_id">' + result[i].id + '</td>')
+            .append('<td id="req_status">' + result[i].status + '</td>')
+            .append('<td id="req_name">' + result[i].name + '</td>')
+            .append('<td id="req_phone">' + result[i].phone + '</td>')
+            .append('<td id="req_email">' + result[i].email + '</td>')
+            .append('<td id="req_comment">' + result[i].comment + '</td>')
+            .append('<td id="req_date">' + result[i].date + '</td>');
 
+        /*Опция действия(-ий) выбирается в зависимости от статуса заявки*/
         if (result[i].status == 'NEW') {
-            jQuery('#tblBody')
-                .append('<td><input class="process" type="button" value="Process" onclick="createRequisitionProcessForm()"></td>')
+            jQuery('#cur_row' + i)
+                .append('<td><input class="process" type="button" value="Process" onclick="processRequisition(this)"></td>')
+        } else if (result[i].status == 'PROCESS') {
+            jQuery('#cur_row' + i).append('<td></td>');
         }
     }
 }
+/*********************************************************************************************************************/
+
--- SandBoxClient/src/main/resources/templates/vm/dashboard/js/dashboard.js
+++ SandBoxClient/src/main/resources/templates/vm/dashboard/js/dashboard.js
@@ -1,3 +1,4 @@
+/* Получение всех актиывных заявок с веб-клиента*/
 function checkRequisitions() {
     createRequisitionTable();
     jQuery.ajax({
@@ -10,4 +11,49 @@ function checkRequisitions() {
             console.log(errorThrown);
         }
     });
+}
+
+/* Изменение статуса заявки:
+* - PROCESS - заявка в обработке
+* - DONE    - заявка обработана*/
+function requisitionStatusUpdate(reqId, newStatus) {
+    jQuery.ajax({
+        type: 'POST',
+        url: '/update_status',
+        data: {
+            req_id: reqId,
+            status : newStatus
+        },
+        success: function () {
+            console.log(reqId + " status updated.")
+        }
+    });
+}
+/**********************************************************************************************************************/
+/* Создание нового клиента для формы обработки заявок:*/
+function createClient() {
+    jQuery.ajax({
+        type: 'POST',
+        url: '/new_client',
+        data: {
+            name: jQuery('#name').val(),
+            surname: jQuery('#surname').val(),
+            phone: jQuery('#phone').val(),
+            email: jQuery('#email').val()
+        },
+        success: function () {
+            jQuery('#name').val('');
+            jQuery('#surname').val('');
+            jQuery('#phone').val('');
+            jQuery('#email').val('');
+
+            /*В случае успешного создания карточки клиента закрываем заявку*/
+            /*requisitionStatusUpdate("DONE");*/
+        }
+    });
+}
+
+/* Получение всех клиентов которые зарегистрированы в базе */
+function checkClients() {
+
 }
