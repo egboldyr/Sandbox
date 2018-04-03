c1c1f3dfdc893dd29b52862f377d230972fe9f22
egboldyr <egboldyr>
2018-04-03 11:26:23 +0300

    DAO (Requisition) Fix: - Excluded type RequisitionStatus.DONE

--- SandboxServer/src/main/java/soap/dao/impl/RequisitionDAOImpl.java
+++ SandboxServer/src/main/java/soap/dao/impl/RequisitionDAOImpl.java
@@ -2,13 +2,16 @@ package soap.dao.impl;
 
 import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Criterion;
 import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import soap.dao.RequisitionDAO;
 import soap.entity.Requisition;
+import soap.entity.enums.RequisitionStatus;
 
 import javax.annotation.PostConstruct;
 import javax.transaction.Transactional;
@@ -80,6 +83,7 @@ public class RequisitionDAOImpl implements RequisitionDAO {
             List<Requisition> requisitions = factory.getCurrentSession()
                                                         .createCriteria(Requisition.class)
                                                         .addOrder(Order.desc("creationDate"))
+                                                        .add(Restrictions.ne("status", RequisitionStatus.DONE))
                                                         .setFirstResult(from)
                                                         .setMaxResults(count)
                                                         .list();
