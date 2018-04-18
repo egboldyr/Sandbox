a681a077bd5b71ad3dbfe7cbb6f469f0e6710237
egboldyr <egboldyr>
2018-04-18 17:14:35 +0300

    SandBoxClient (Module) - (FrontEnd) dashboard.html - Update Added: - Navigation buttons ("Prev", "Next") for Courses.

--- SandBoxClient/src/main/resources/templates/vm/dashboard/js/courses.js
+++ SandBoxClient/src/main/resources/templates/vm/dashboard/js/courses.js
@@ -4,18 +4,107 @@ function genCoursesAndGroupsInterface() {
 
 function createCourseInterface() {
     jQuery('#workspace').empty()
-        .append('<div id="courses_ui"></div>');
+        .append('<p><span id="courses_ui"></span></p>')
+        .append('<p><span id="groups_ui"></span></p>');
+
+    /* Интерфейс для работы с курсами (Добавление новых курсов)*/
     jQuery('#courses_ui')
-        .append('<div id="courses_tab"><table id="courses"></table></div>')
+        .append('<div id="courses_tab"><table id="courses"></table>' +
+                                      '<input id="prevCourses" type="button" value="Prev" onclick="prevCourses()">' +
+                                      '<input id="nextCourses" type="button" value="Next" onclick="nextCourses()"></div>')
         .append('<div id="courses_form"><form id="course_form"></form></div>');
     jQuery('#courses')
         .append('<thead><tr id="header"></tr></thead>');
     jQuery('#header')
         .append('<th>ID</th>')
-        .append('<th>TITLE</th>');
+        .append('<th>TITLE</th>')
+        .append('<th>ACTIONS</th>');
     jQuery('#course_form')
         .append('<p>Title<input id="course_title" type="text"></p>')
-        .append('<input type="button" value="Add Course" onclick="return createCourse()">');
+        .append('<input type="button" value="Add Course" onclick="return createCourse()">')
+        .append('<p>Course ID<input id="crs_id" type="text" disabled="true"></p>')
+        .append('<p>Title<input id="crs_title" type="text" disabled="true"></p>');
+
+    /* Интерфейс для работы с группами */
+    jQuery('#groups_ui')
+        .append('<div id="groups_view"></div>')
+        .append('<div id="group_students"></div>');
+
+    jQuery('#groups_view')
+        .append('<div id="groups_tab"><table id="groups"></table></div>')
+        .append('<div id="groups_form"><form id="group_form"></form></div>');
+
+    jQuery('#group_students')
+        .append('<div id="students_tab"><table id="students"></table></div>')
+        .append('<div id="clients_tab"><table id="clients"></table></div>');
+
+    /*Интерфейс добавления/изменения групп (Создание группы и редактирование основной информации)*/
+    jQuery('#groups')
+        .append('<thead><tr id="grp_header"></tr></thead>')
+        .append('<tbody id="tblGroupsBody"></tbody>');
+    jQuery('#grp_header')
+        .append('<th>ID</th>')
+        .append('<th>TITLE</th>')
+        .append('<th>BEGIN_DATE</th>')
+        .append('<th>END_DATE</th>')
+        .append('<th>ACTIONS</th>');
+
+    for (var i = 0; i < 5; i++) {
+        jQuery('#tblGroupsBody')
+            .append('<tr id="cur_row' + i + '" class="item"></tr>');
+        jQuery('#cur_row' + i)
+            .append('<td id="group_id">-</td>')
+            .append('<td id="group_title">-</td>')
+            .append('<td id="group_begin">-</td>')
+            .append('<td id="group_end">-</td>')
+            .append('<td id="group_action">-</td>');
+    }
+
+    jQuery('#group_form')
+        .append('<p>Group ID<input id="grp_id" type="text" disabled="true"></p>')
+        .append('<p>Title <input id="grp_title" type="text"></p>')
+        .append('<p>Begin date<input id="grp_begin" type="date"></p>')
+        .append('<p>End date<input id="grp_end" type="date"></p>')
+        .append('<input type="button" value="Add" onclick="return createNewGroup()">');
+
+    /*Интерфейс добавления/удаления студентов (Формирование групп на основе существующих заявок)*/
+    jQuery('#students')
+        .append('<thead><tr id="std_header"></tr></thead>')
+        .append('<tbody id="tblStudentsBody"></tbody>');
+    jQuery('#std_header')
+        .append('<th>ID</th>')
+        .append('<th>NAME</th>')
+        .append('<th>SURNAME</th>')
+        .append('<th>ACTIONS</th>');
+
+    for (var i = 0; i < 5; i++) {
+        jQuery('#tblStudentsBody')
+            .append('<tr id="cur_student_row' + i + '" class="item"></tr>');
+        jQuery('#cur_student_row' + i)
+            .append('<td id="student_id">-</td>')
+            .append('<td id="student_name">-</td>')
+            .append('<td id="student_surname">-</td>')
+            .append('<td id="student_action">-</td>');
+    }
+
+    jQuery('#clients')
+        .append('<thead><tr id="clt_header"></tr></thead>')
+        .append('<tbody id="tblClientsBody"></tbody>');
+    jQuery('#clt_header')
+        .append('<th>ID</th>')
+        .append('<th>NAME</th>')
+        .append('<th>SURNAME</th>')
+        .append('<th>ACTIONS</th>');
+
+    for (var i = 0; i < 5; i++) {
+        jQuery('#tblClientsBody')
+            .append('<tr id="cur_client_row' + i + '" class="item"></tr>');
+        jQuery('#cur_client_row' + i)
+            .append('<td id="client_id">-</td>')
+            .append('<td id="client_name">-</td>')
+            .append('<td id="client_surname">-</td>')
+            .append('<td id="client_action">-</td>');
+    }
 }
 
 function processCourses(jsonResult) {
@@ -28,6 +117,16 @@ function processCourses(jsonResult) {
         jQuery('#tblBody').append('<tr id="cur_row' + i + '" class="item"></tr>');
         jQuery('#cur_row' + i)
             .append('<td id="crs_id">' + result[i].id + '</td>')
-            .append('<td id="crs_title">' + result[i].title + '</td>');
+            .append('<td id="crs_title">' + result[i].title + '</td>')
+            .append('<td id="crs_groups"><input class="process" type="button" value="View groups" onclick="return viewGroups(this)"></td>');
     }
+}
+
+function viewGroups(btn) {
+    var course = btn.parentNode.parentNode;
+    /*crs_id crs_title*/
+}
+
+function createNewGroup() {
+    
 }
