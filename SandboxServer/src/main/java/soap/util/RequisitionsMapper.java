37512d52709a769d0b7a801c8ddb3fc456bf8714
egboldyr <egboldyr>
2018-05-07 10:48:16 +0300

    MapStruct - Maven Settings

---
+++ SandboxServer/src/main/java/soap/util/RequisitionsMapper.java
@@ -0,0 +1,16 @@
+package soap.util;
+
+import org.mapstruct.Mapper;
+import soap.dto.RequisitionDTO;
+import soap.entity.Requisition;
+
+/**
+ * Created by EGBoldyr on 07.05.18.
+ */
+
+@Mapper(componentModel = "spring")
+public interface RequisitionsMapper {
+
+    RequisitionDTO requisitionToRequisitionDto(Requisition requisition);
+
+}
