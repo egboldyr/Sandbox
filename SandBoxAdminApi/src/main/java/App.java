86f7703cdfb9d25f4a61520beb2433fe788ea278
egboldyr <egboldyr>
2018-05-10 14:30:14 +0300

    Admin Rest API - Application configuration update

--- SandBoxAdminApi/src/main/java/App.java
+++ SandBoxAdminApi/src/main/java/App.java
@@ -1,11 +1,13 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by EGBoldyr on 10.05.18.
  */
 
 @SpringBootApplication
+@ComponentScan("rest")
 public class App {
 
     public static void main(String[] args) {
