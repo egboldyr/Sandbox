b2da5f860de646cbf9f4fbf5706907d019df32c2
egboldyr <egboldyr>
2018-05-10 11:01:01 +0300

    Admin Rest API - Initial Commit

---
+++ SandBoxAdminApi/src/main/java/App.java
@@ -0,0 +1,14 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by EGBoldyr on 10.05.18.
+ */
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
