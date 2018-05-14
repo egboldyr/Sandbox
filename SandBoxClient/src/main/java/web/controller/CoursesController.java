33ef2f041b96579d18d0801c737fe1019088d782
egboldyr <egboldyr>
2018-05-14 13:22:17 +0300

    Redis Cache - Refactor CoursesController

--- SandBoxClient/src/main/java/web/controller/CoursesController.java
+++ SandBoxClient/src/main/java/web/controller/CoursesController.java
@@ -2,14 +2,13 @@ package web.controller;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import web.jaxws.CourseDTO;
-import web.jaxws.CourseService;
-import web.jaxws.CourseWebService;
+import web.service.CourseCache;
 
-import javax.annotation.PostConstruct;
 import java.util.List;
 
 /**
@@ -24,24 +23,18 @@ public class CoursesController {
     private static final String URL_GET_COURSES          = "/get_courses";
     private static final String URL_GET_COURSES_PART     = "/part_courses";
 
-    private Integer from;
-    private CourseWebService courseWS;
-
-    @PostConstruct
-    private void initialize() {
-        CourseService service = new CourseService();
-        courseWS = service.getCoursePort();
-    }
+    @Autowired
+    private CourseCache courseCache;
 
     @RequestMapping(value = URL_NEW_COURSE, method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
     public void newCourse(@RequestParam("title") String title) {
-        courseWS.newCourse(title);
+        courseCache.newCourse(title);
     }
 
     @RequestMapping(value = URL_FIND_COURSE_BY_TITLE, method = RequestMethod.POST)
     public @ResponseBody String findByTitle(@RequestParam("title") String title) {
-        CourseDTO course = courseWS.findByTitle(title);
+        CourseDTO course = courseCache.findByTitle(title);
         JSONObject item = new JSONObject();
         item.put("id", course.getId());
         item.put("title", course.getTitle());
@@ -50,15 +43,7 @@ public class CoursesController {
 
     @RequestMapping(value = URL_GET_COURSES_PART, method = RequestMethod.POST)
     public @ResponseBody String getCoursesPart(@RequestParam("action") String action) {
-        if (action.equals("PREV") && from > 0) {
-            from--;
-        } else if (action.equals("NEXT")) {
-            from++;
-        } else if (action.equals("UPLOAD")) {
-            from = 0;
-        }
-
-        List<CourseDTO> courses = courseWS.findCoursesPart(from).getItem();
+        List<CourseDTO> courses = courseCache.getCoursesPage(action);
         JSONArray body = new JSONArray();
         for (CourseDTO course : courses) {
             JSONObject item = new JSONObject();
@@ -71,7 +56,7 @@ public class CoursesController {
 
     @RequestMapping(value = URL_GET_COURSES, method = RequestMethod.GET)
     public @ResponseBody String getCourses() {
-        List<CourseDTO> courses = courseWS.allCourses().getItem();
+        List<CourseDTO> courses = courseCache.getAllCourses();
         JSONArray body = new JSONArray();
         for (CourseDTO course : courses) {
             JSONObject item = new JSONObject();
